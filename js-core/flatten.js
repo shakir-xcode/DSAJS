54823d5e4890df9fbb2409f3a2ec71476eccea3b
Shakir <shaker88999@gmail.com>
2024-08-01 22:28:01 +0530

    flatten array using reduce method and recursive approach

---
+++ js-core/flatten.js
@@ -0,0 +1,13 @@
+const nestedArray = [1, [2, 3], 4, [5, [6, 7]]];
+
+const flatten = list => {
+	return list.reduce((acc, val)=>{
+		if(typeof val !== "number") {
+			acc.push(...flatten(val));
+		} else
+			acc.push(val);
+		return acc;
+	},[])
+}
+
+console.log(flatten(nestedArray));
